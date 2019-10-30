e5a7286631a424d316acec3357ffd13df5cbf1f2
Biolin <shex.ck@gmail.com>
2019-10-30 09:03:02 +0800

    evaluation code on YTF dataset

---
+++ data/datasets_YTF.py
@@ -0,0 +1,96 @@
+import cv2
+import glob
+import os.path as op
+
+import numpy as np
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms, datasets
+# from .align import Alignment
+
+
+class YTFCroppedFacesDataset(Dataset):
+    """
+        This dataset loads all faces available in YTF, (align them,)
+        and transform them into tensors.
+        The path for that face is output along with its tensor.
+        This is for models to compute all faces' features and store them
+        into disks, otherwise the verification testing set contains too many
+        repeated faces that should not be computed again and again.
+
+        Note: I do not align the dataset because I downloaded the cropped
+        version from https://github.com/huangyangyu/SeqFace#how-to-test,
+        which is aligned by the author.
+    """
+    def __init__(self, data_root, is_ijbb=True):
+        self.img_dir = data_root
+        self.transforms = transforms.Compose([
+            transforms.Resize([112, 112]),
+            transforms.ToTensor(),
+            transforms.Normalize([.5, .5, .5], [.5, .5, .5]),
+        ])
+        self.imgs_list = self.loadImgPaths(self.img_dir)
+        # self.alignment = Alignment()
+
+    def loadImgPaths(self, img_dir):
+        # e.g. dir/Ted_Turner/3/aligned_detect_3.344.jpg
+        paths = glob.glob(op.join(img_dir, '*', '*', '*.jpg'))
+        return paths
+
+    def __getitem__(self, idx):
+        img_path = self.imgs_list[idx]
+        try:
+            img = cv2.imread(img_path)
+            img = Image.fromarray(img)
+        except Exception as e:
+            print(e)
+            print(img_path)
+        tensor = self.transforms(img)
+        return {
+            "tensor": tensor,
+            "path": img_path,
+        }
+
+    def __len__(self):
+        return len(self.imgs_list)
+
+
+class YTFVerificationPathDataset(Dataset):
+    """
+        This dataset read the match file of verification set in ijb_dataset_root
+        (in the `meta` directory, the filename is sth. like
+        "ijbc_template_pair_label.txt") and output the cropped faces'
+        paths of both enroll_template and verif_template for each match.
+
+        Models outside can use the path information to read their stored
+        features and compute the similarity score of enroll_template and
+        verif_template.
+    """
+    def __init__(self, pair_file, img_dir, leave_ratio=1.0):
+        self.match = pd.read_csv(pair_file, dtype=str)
+        self.img_dir = img_dir
+        if leave_ratio < 1.0:  # shrink the number of verified pairs
+            indice = np.arange(len(self.match))
+            np.random.seed(0)
+            np.random.shuffle(indice)
+            left_number = int(len(self.match) * leave_ratio)
+            self.match = self.match.iloc[indice[:left_number]]
+
+    def __getitem__(self, idx):
+        def path_suffixes(id_str):
+            paths = glob.glob(op.join(self.img_dir, id_str, '*'))
+            paths = [op.join(*path.split('/')[-3:]) for path in paths]
+            return paths
+        id1 = self.match.iloc[idx][" first name"].strip()
+        id2 = self.match.iloc[idx][" second name"].strip()
+        return {
+            "enroll_template_id": id1,
+            "verif_template_id": id2,
+            "enroll_path_suffixes": path_suffixes(id1),
+            "verif_path_suffixes": path_suffixes(id2),
+            "is_same": self.match.iloc[idx][" is same:"].strip()
+        }
+
+    def __len__(self):
+        return len(self.match)
---
+++ run_models_on_YTF.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import time
+import os.path as op
+import argparse
+from torchvision import transforms as trans
+from tqdm import tqdm
+import torch
+import numpy as np
+from IPython.display import display
+
+from Learner import face_learner
+from config import get_config
+from utils import hflip_batch, cosineDim1
+from data.datasets_YTF import YTFCroppedFacesDataset#, IJBCVerificationPathDataset
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+def process_batch_original(batch, tta=False):
+    if tta:
+        fliped = hflip_batch(batch)
+        feat_orig = learner.model.get_original_feature(batch.to(conf.device))
+        feat_flip = learner.model.get_original_feature(fliped.to(conf.device))
+        feat = (feat_orig + feat_flip) / 2
+    else:
+        feat = learner.model.get_original_feature(batch.to(conf.device))
+    return feat
+
+
+def process_batch_xcos(batch, tta=False):
+    if tta:
+        fliped = hflip_batch(batch)
+        flattened_feature1, feat_map1 = learner.model(batch.to(conf.device))
+        flattened_feature2, feat_map2 = learner.model(fliped.to(conf.device))
+        feat_map = (feat_map1 + feat_map2) / 2
+        flattened_feature = (flattened_feature1 + flattened_feature2) / 2
+    else:
+        flattened_feature, feat_map = learner.model(batch.to(conf.device))
+    return flattened_feature, feat_map
+
+
+# ## Init Learner and conf
+conf = get_config(training=False)
+conf.batch_size=20 # Why bs_size can only be the number that divide 6000 well?
+learner = face_learner(conf, inference=True)
+# # Process YTF data and save them
+dataset_name = 'YTF'
+feat_root_dir = 'work_space/YTF_features_aligned'
+os.makedirs(feat_root_dir, exist_ok=True)
+loader = torch.utils.data.DataLoader(
+    # YTFCroppedFacesDataset('data/YTF_aligned_SeqFace'),
+    YTFCroppedFacesDataset('/home/r07944011/datasets_ssd/face/YTF/align_InsightFace/cropped_and_aligned'),
+    batch_size=1
+)
+
+# ## Original model
+def run_irse50_YTF():
+    learner.load_state(conf, 'ir_se50.pth', model_only=True,
+                       from_save_folder=True, strict=False,
+                       model_atten=False)
+    learner.model.eval()
+    learner.model.returnGrid = True  # Remember to reset this before return!
+    learner.model_attention.eval()
+
+    dst_dir = op.join(feat_root_dir, f'{dataset_name}_ir_se50')
+    os.makedirs(dst_dir, exist_ok=True)
+
+    with torch.no_grad():
+        for i, batch in tqdm(enumerate(loader), total=len(loader)):
+            tensor = batch['tensor']
+            src_path = batch['path']
+            # if i % 200 == 0:
+            #     print(f'Processing {src_path[0]}')
+            os.makedirs(op.join(dst_dir, *src_path[0].split('/')[-3:-1]), exist_ok=True)
+            target_path = op.join(dst_dir, *src_path[0].split('/')[-3:]) + '.npy'
+            if op.exists(target_path):
+                if i % 200 == 0:
+                    print(f"Skipping {target_path} because it exists.")
+                continue
+            feat = process_batch_original(tensor, tta=True)
+            np.save(target_path, feat.cpu().numpy())
+
+
+# ## Our model
+def run_ours_YTF(model_name):
+    learner.load_state(
+        conf, f'{model_name}.pth',
+        model_only=True, from_save_folder=True, strict=True, model_atten=True)
+    learner.model.eval()
+    learner.model.returnGrid = True  # Remember to reset this before return!
+    learner.model_attention.eval()
+
+    dst_dir = op.join(feat_root_dir, f'{dataset_name}_{model_name}')
+    os.makedirs(dst_dir, exist_ok=True)
+
+    with torch.no_grad():
+        for i, batch in tqdm(enumerate(loader), total=len(loader)):
+            tensor = batch['tensor']
+            src_path = batch['path']
+            os.makedirs(op.join(dst_dir, *src_path[0].split('/')[-3:-1]), exist_ok=True)
+            target_path = op.join(dst_dir, *src_path[0].split('/')[-3:]) + '.npz'
+            # if i % 2000 == 0:
+            #    print(f'Processing {src_path[0]} \nSaving to {target_path}')
+            if op.exists(target_path):
+                if i % 2000 == 0:
+                    print(f"Skipping {target_path} because it exists.")
+                continue
+            flattened_feature, feat_map = process_batch_xcos(tensor, tta=True)
+            np.savez(target_path, flattened_feature=flattened_feature.cpu().numpy(),
+                     feat_map=feat_map.cpu().numpy())
+
+
+model_names = [
+    # '2019-09-01-15-30_accuracy:0.9946666666666667_step:218346_CosFace',
+    '2019-09-02-08-21_accuracy:0.9968333333333333_step:436692_CosFace',
+    # '2019-08-25-14-35_accuracy:0.9931666666666666_step:218349_None',
+    '2019-08-30-07-36_accuracy:0.9953333333333333_step:655047_None'
+]
+
+
+def run_all_models():
+    for model_name in model_names:
+        print(f'-------- Runing on model {model_name} --------')
+        run_ours_YTF(model_name)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='feature extraction')
+    # general
+    parser.add_argument('--model', default='all', help='model to test')
+    args = parser.parse_args()
+    if(args.model == 'all'):
+        run_all_models()
+    elif(args.model == 'irse50'):
+        run_irse50_YTF()
+    elif(args.model == 'cosface'):
+        run_ours_YTF(model_names[0])
+    elif(args.model == 'arcface'):
+        run_ours_YTF(model_names[1])
+    else:
+        print('Unknown model name!')
