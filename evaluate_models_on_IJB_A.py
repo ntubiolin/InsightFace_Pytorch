e420e24556ec682a38e978b18eec96f95b7a2405
Biolin <shex.ck@gmail.com>
2019-11-03 12:58:33 +0800

    modulize IJB-A eval code

--- evaluate_models_on_IJB_A.py
+++ evaluate_models_on_IJB_A.py
@@ -25,50 +25,60 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
 # ## Init Learner and conf
-
-# In[2]:
-
-
 conf = get_config(training=False)
 conf.batch_size=20 # Why bs_size can only be the number that divide 6000 well?
 learner = face_learner(conf, inference=True)
 
 
-# In[3]:
-
-
 def l2normalize(x, ord=2, axis=None):
-    return x / np.linalg.norm(x, ord=ord, axis=axis)
+    '''
+    x: (# of bs, 512)
+    '''
+    norm_of_batches = np.linalg.norm(x, ord=ord, axis=axis)
+    norm_of_batches = norm_of_batches.reshape(x.shape[0], 1)
+    normalized_x = x / norm_of_batches
+    return normalized_x
 
 
 def score_fn_original(feat1, feat2):
     # feat1, feat2.shape (512, x), x is the # of imgs
-    return l2normalize(feat1).dot(l2normalize(feat2).T).mean()
+    return l2normalize(feat1,axis=1).dot(l2normalize(feat2,axis=1).T).mean()
 
 
-def score_fn_ours(feat_map1, feat_map2, learner, attention_strategy='learned', attention_weight=None):
+def score_fn_ours(feat_map1s, feat_map2s, learner, attention_strategy='learned', attention_weight=None):
+    '''
+    feat_map1s & feat_map2s: (# of bs, 32, 7, 7)
+    '''
+    xCoses = []
+    feat_map1s = torch.tensor(feat_map1s).to(conf.device)
+    feat_map2s = torch.tensor(feat_map2s).to(conf.device)
     with torch.no_grad():
-        assert len(feat_map1.shape) == 3  # [c, w, h]
-        c, w, h = feat_map1.shape
-        feat_map1 = torch.tensor(feat_map1).unsqueeze(0).to(conf.device)
-        feat_map2 = torch.tensor(feat_map2).unsqueeze(0).to(conf.device)
-        if attention_strategy == 'learned':
-            attention = None
-        elif attention_strategy == 'uniform':
-            learner.model_attention.eval()
-            attention = torch.ones([1, 1, w, h])  # [batch, c, w, h]
-            attention /= attention.sum()
-            attention = attention.to(conf.device)
-        elif attention_strategy == 'fixed':
-            assert attention_weight is not None
-            assert attention_weight.shape[0] == w and attention_weight.shape[1] == h
-            attention = torch.tensor(attention_weight).view(
-                [1, 1, attention_weight.shape[0], attention_weight.shape[1]]
-            ).type(feat_map1.type())
-        else:
-            raise NotImplementedError
-        xCos, attention, cos_patched = learner.get_x_cosine(feat_map1, feat_map2, attention)
-    return xCos.cpu().numpy()
+        for feat_map1 in feat_map1s:
+            for feat_map2 in feat_map2s:
+                assert len(feat_map1.shape) == 3  # [c, w, h]
+                c, w, h = feat_map1.shape
+                _feat_map1 = feat_map1.unsqueeze(0)
+                _feat_map2 = feat_map2.unsqueeze(0)
+                if attention_strategy == 'learned':
+                    attention = None
+                elif attention_strategy == 'uniform':
+                    learner.model_attention.eval()
+                    attention = torch.ones([1, 1, w, h])  # [batch, c, w, h]
+                    attention /= attention.sum()
+                    attention = attention.to(conf.device)
+                elif attention_strategy == 'fixed':
+                    assert attention_weight is not None
+                    assert attention_weight.shape[0] == w and attention_weight.shape[1] == h
+                    attention = torch.tensor(attention_weight).view(
+                        [1, 1, attention_weight.shape[0], attention_weight.shape[1]]
+                    ).type(_feat_map1.type())
+                else:
+                    raise NotImplementedError
+                xCos, attention, cos_patched = learner.get_x_cosine(_feat_map1, _feat_map2, attention)
+                xCoses.append(xCos.cpu().numpy())
+    xCos_mean = np.array(xCoses).mean()
+    return xCos_mean
+
 
 def evaluate_and_plot(scores, is_sames, nrof_folds=10, dataset_name='AR Face', display_roc_curve=True):
     accuracy, best_threshold, roc_curve_tensor = learner.evaluate_and_plot_roc(scores, is_sames, nrof_folds=10)
@@ -77,24 +87,6 @@ def evaluate_and_plot(scores, is_sames, nrof_folds=10, dataset_name='AR Face', d
         display(trans.ToPILImage()(roc_curve_tensor))
     return accuracy
 
-
-# # IJB-A
-feat_root_dir = 'work_space/IJB_A_features/IJB-A_full_template/split1/'
-# In[4]:
-
-
-all_scores = {}
-all_acc = {}
-
-def record_scores_and_acc(scores, acc, name, name2=None):
-    all_scores[name] = scores
-    all_acc[name] = acc
-
-shuffle_order = np.arange(len(IJBAVerificationDataset()))
-np.random.shuffle(shuffle_order)
-
-
-# In[5]:
 
 
 '''
@@ -106,6 +98,7 @@ def _get_feature(fname):
 
 def run_IJBA_verification(feat_dir, score_fn, score_fn_kargs, shuffle_order,
                           learner=None, attention_strategy=None, ):
+    print(feat_dir)
     is_sames = []
     scores = []
     init_time = time.time()
@@ -123,84 +116,108 @@ def run_IJBA_verification(feat_dir, score_fn, score_fn_kargs, shuffle_order,
 
     scores = np.array(scores).squeeze()
     is_sames = np.array(is_sames).squeeze().astype(np.bool)
+
     return scores, is_sames
 
 
-# In[6]:
+def eval3TAR(scores, is_sames):
+    for far_target in [1e-1, 1e-2, 1e-3]:
+        mean_tar = calculate_val_by_diff(
+            thresholds='default',
+            dist=(1 - scores),
+            actual_issame=is_sames,
+            far_target=far_target, ret_acc=True,
+            nrof_folds=5
+                                        )[0]
+        print(f"TAR@FAR{far_target:.4f}:{mean_tar}")
 
 
-scores, is_sames = run_IJBA_verification(
-    feat_dir=os.path.join(feat_root_dir, 'ir_se50/'),
-    score_fn=score_fn_original, score_fn_kargs={},
-    shuffle_order=shuffle_order
-)
-print(np.histogram(scores), np.histogram(is_sames))
-acc = evaluate_and_plot(scores, is_sames, nrof_folds=10, dataset_name='IJBC')
-all_scores['original_ArcFace'] = scores
-all_acc['original_ArcFace'] = acc
-
-
-# In[9]:
-
-
-for far_target in [1e-1, 1e-2, 1e-3]:
-    mean_tar = calculate_val_by_diff(
-        thresholds='default', dist=(1 - scores), actual_issame=is_sames, far_target=0.001, ret_acc=True,
-        nrof_folds=5
-    )[0]
-    print("TAR@FAR{far_target:.f}:{mean_tar}")
-# calculate_val_far()
-
-
-# In[ ]:
-
-
-evaluate_and_plot_roc
-
-
-# In[11]:
-
-
-model_name = '2019-09-02-08-21_accuracy:0.9968333333333333_step:436692_CosFace'
-fixed_weight = np.load(f'/tmp3/biolin/data/insightFace_pytorch/{model_name}.npy')
-fixed_weight /= fixed_weight.sum()
-
-for attention_strategy in ['uniform', 'learned', 'fixed']:
-    print(f'===== attention_strategy: {attention_strategy} =====')
+def evaluate_irse50_IJB_A(feat_root_dir):
+    # Run evaluation
+    model_name = 'ir_se50'
     scores, is_sames = run_IJBA_verification(
         feat_dir=os.path.join(feat_root_dir, f'{model_name}'),
-        score_fn=score_fn_ours,
-        score_fn_kargs={'learner': learner, 'attention_strategy': attention_strategy,
-                        'attention_weight': fixed_weight},
+        score_fn=score_fn_original, score_fn_kargs={},
         shuffle_order=shuffle_order
     )
-    acc = evaluate_and_plot(scores, is_sames, nrof_folds=10, dataset_name='IJBC')
 
-    all_scores[f'{model_name}_{attention_strategy}'] = scores
-    all_acc[f'{model_name}_{attention_strategy}'] = acc
+    acc = evaluate_and_plot(scores, is_sames,
+                            nrof_folds=10,
+                            dataset_name=f'IJB-A {model_name}')
+
+    all_scores['original_ArcFace_irse50'] = scores
+    all_acc['original_ArcFace_irse50'] = acc
+    eval3TAR(scores, is_sames)
 
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='evaluation given the features')
-#     # general
-#     parser.add_argument('--model', default='all', help='model to test')
-#     parser.add_argument('--feat_root_dir',
-#                         default='work_space/YTF_features_aligned',
-#                         help='where the features are stored')
-#     parser.add_argument('--cmp_strategy', default='mean_comparison',
-#                         help='mean_comparison or compare_only_first_img')
-#     args = parser.parse_args()
-#
-#     feat_root_dir = args.feat_root_dir
-#     comparison_strategy = args.cmp_strategy
-#     print('>>>>> Comparison Strategy:', comparison_strategy)
-#     if(args.model == 'all'):
-#         eval_all_models()
-#     elif(args.model == 'irse50'):
-#         evaluate_irse50_YTF()
-#     elif(args.model == 'cosface'):
-#         evaluate_ours_YTF(model_names[0])
-#     elif(args.model == 'arcface'):
-#         evaluate_ours_YTF(model_names[1])
-#     else:
-#         print('Unknown model name!')
+def evaluate_ours_IJB_A(model_name, feat_root_dir):
+    fixed_weight = np.load(f'data/correlation_weights/{model_name}.npy')
+    fixed_weight /= fixed_weight.sum()
+
+    for attention_strategy in ['uniform', 'learned', 'fixed']:
+        print(f'===== attention_strategy: {attention_strategy} =====')
+        scores, is_sames = run_IJBA_verification(
+            feat_dir=os.path.join(feat_root_dir, f'{model_name}'),
+            score_fn=score_fn_ours,
+            score_fn_kargs={'learner': learner,
+                            'attention_strategy': attention_strategy,
+                            'attention_weight': fixed_weight},
+            shuffle_order=shuffle_order
+        )
+        acc = evaluate_and_plot(scores, is_sames,
+                                nrof_folds=10,
+                                dataset_name=model_name[-7:])
+
+        all_scores[f'{model_name}_{attention_strategy}'] = scores
+        all_acc[f'{model_name}_{attention_strategy}'] = acc
+        eval3TAR(scores, is_sames)
+
+
+if __name__ == '__main__':
+    # # IJB-A
+    # feat_root_dir = 'work_space/IJB_A_features/IJB-A_full_template/split1/'
+    cosface_name = \
+        '2019-09-02-08-21_accuracy:0.9968333333333333_step:436692_CosFace'
+    arcface_name = \
+        '2019-08-30-07-36_accuracy:0.9953333333333333_step:655047_None'
+    model_names = [cosface_name, arcface_name]
+
+    all_scores = {}
+    all_acc = {}
+
+    def record_scores_and_acc(scores, acc, name, name2=None):
+        all_scores[name] = scores
+        all_acc[name] = acc
+
+    # XXX Why do we need the shuffle order？ Due to the sample ratio issue?
+    shuffle_order = np.arange(len(IJBAVerificationDataset(ijba_data_root='data/IJB-A/')))
+    np.random.shuffle(shuffle_order)
+
+    def eval_all_my_models(model_names, feat_root_dir):
+        for model_name in model_names:
+            evaluate_ours_IJB_A(model_name, feat_root_dir)
+
+    # Parse the arguments
+    parser = argparse.ArgumentParser(description='evaluation given the feats')
+    parser.add_argument('--model', default='all', help='model to test')
+    parser.add_argument('--feat_root_dir',
+                        default='work_space/IJB_A_features/IJB-A_full_template/split1',
+                        help='where the features are stored')
+    # parser.add_argument('--cmp_strategy', default='mean_comparison',
+    #                     help='mean_comparison or compare_only_first_img')
+    args = parser.parse_args()
+
+    feat_root_dir = args.feat_root_dir
+    # comparison_strategy = args.cmp_strategy
+    # print('>>>>> Comparison Strategy:', comparison_strategy)
+    if(args.model == 'all'):
+        eval_all_my_models(model_names, feat_root_dir)
+    elif(args.model == 'irse50'):
+        evaluate_irse50_IJB_A(feat_root_dir)
+    elif(args.model == 'cosface'):
+        evaluate_ours_IJB_A(cosface_name, feat_root_dir)
+    elif(args.model == 'arcface'):
+        evaluate_ours_IJB_A(arcface_name, feat_root_dir)
+    else:
+        print('Unknown model name!')
+
